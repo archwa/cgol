f72387bc459423a389ade7323fe87cdb49806e20
Arthur C. Watkins <archwa@protonmail.ch>
2021-06-01 18:41:42 -0500

    significantly improved the code and algorithm; now, all static computations (e.g. neighbor indices) are performed in game initialization; and nearly all update code is in a single step function to reduce unnecessary function call overhead; and we try to not create as many new objects as before, e.g., arrays of indices are no longer created, because dynamic memory allocation is computationally expensive; and global variables are no longer 'const' to allow for user modification

--- src/js/cgol.js
+++ src/js/cgol.js
@@ -1,57 +1,29 @@
-const mult = 8;
-const freq = 24;
+// global variables undesirable, should improve
+let CGOLObj, ctx;
+let freq = 60, mult = 6;
 
-var g, ctx;
-
-// THE sample distribution function
 function sample_dist() {
   // 0.2 bias to left
   return Math.random() - 0.2 >= 0.5 ? 1 : 0
 }
 
-// Box-Muller (other dists?)
-function randn_bm() {
-  var u = 1 - Math.random(), v = 1 - Math.random();
-  return Math.abs((Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v ) + 3.5) / 7);
-}
-
-
-// these versions of add and sub work if only add/sub 1 (save time)
-// for torus topology
-class Galois1 {
-  
-  static add(a, b, n) {
-    const sum = a + b;
-    return sum == n ? 0 : sum; // for b = 1
-    //return (a + b) % n; // for all b
-  }
-
-  static sub(a, b, n) {
-    const diff = a - b;
-    return diff == -1 ? diff + n : diff; // for b = 1
-    //return diff < 0 ? (diff % n) + n : diff; // for all b
-  }
-  
-}
-
 class CGOL {
 
   constructor(w, h, f) {
-    const { grid, living } = CGOL.makeGrid(w, h);
+    this.w = w;
+    this.h = h;
+    const { grid, livingCells } = CGOL.makeGrid(w, h);
     this.grid = grid;
-    this.living = living;
-    this.toVisit = CGOL.findToVisit(this.grid, this.living);
-    this.diffCells = [];
+    this.livingCells = livingCells;
 
     this.isStopped = false;
     this.isPaused = true;
     this.setFreq(f);
 
-    for(let j = 0; j < h; j++) {
-      for(let i = 0; i < w; i++) {
-        const cellState = this.grid[j][i];
-        ctx.fillStyle = cellState ? "#fff" : "#000";
-        ctx.fillRect(mult*i, mult*j, mult, mult);
+    for(let i = 0; i < h; i++) {
+      for(let j = 0; j < w; j++) {
+        ctx.fillStyle = this.grid[i][j]["alive"] ? "#fff" : "#000";
+        ctx.fillRect(mult*j, mult*i, mult, mult);
       }
     }
   }
@@ -62,9 +34,11 @@ class CGOL {
   }
 
   start() {
-    this.step();
-    this.print();
-    window.requestAnimationFrame(this.loop.bind(this));
+    if(!this.isStopped) {
+      this.step();
+      this.print();
+      window.requestAnimationFrame(this.loop.bind(this));
+    }
   }
 
   stop() {
@@ -102,146 +76,146 @@ class CGOL {
     this.isPaused = true;
   }
 
-  static findToVisit(grid, living) {
-    const h = grid.length;
-    const w = grid[0].length;
-    const visitMap = {};
-    const toVisit = [];
-
-    for(let i = 0; i < living.length; i++) {
-      const cell = living[i];
-      const x = cell[0], y = cell[1];
-
-      // partial neighbor indices
-      const left =  Galois1.sub(x, 1, w);
-      const right = Galois1.add(x, 1, w);
-      const above = Galois1.sub(y, 1, h);
-      const below = Galois1.add(y, 1, h);
-
-      const indices = [
-        [left, above], [x, above], [right, above],
-        [left,   y  ], [x,   y  ], [right,   y  ],
-        [left, below], [x, below], [right, below]
-      ];
-
-      // only push indices not yet seen
-      indices.forEach(j => {
-        if(!visitMap[j[1]]) {
-          visitMap[j[1]] = {};
-          visitMap[j[1]][j[0]] = true;
-          toVisit.push(j);
-        }
-        
-        else if(!visitMap[j[1]][j[0]]) {
-          visitMap[j[1]][j[0]] = true;
-          toVisit.push(j);
-        }
-      });
-    }
-    
-    return toVisit;
-  }
-
   static makeGrid(w, h) {
-    const grid = [];
-    const living = [];
+    const grid = [], livingCells = [];
+    let i, j, left, right, above, below;
 
-    for(let j = 0; j < h; j++) {
+    for(i = 0; i < h; i++) {
       grid.push([]);
-      for(let i = 0; i < w; i++) {
-        grid[j][i] = sample_dist();
-        if(grid[j][i]) {
-          living.push([i, j]);
-        }
+
+      for(j = 0; j < w; j++) {
+        grid[i][j] = {};
+      }
+    }
+
+    for(i = 0; i < h; i++) {
+      for(j = 0; j < w; j++) {
+        left  = j - 1;
+        right = j + 1;
+        above = i - 1;
+        below = i + 1;
+
+        if(left  == -1) left = left + w;
+        if(right ==  w) right = 0;
+        if(above == -1) above = above + h;
+        if(below ==  h) below = 0;
+
+        grid[i][j]["x"] = j;
+        grid[i][j]["y"] = i;
+        grid[i][j]["alive"] = sample_dist();
+        grid[i][j]["visited"] = false;
+        grid[i][j]["neighbors"] = [
+          grid[above][left], grid[above][  j  ], grid[above][right],
+          grid[  i  ][left],                     grid[  i  ][right],
+          grid[below][left], grid[below][  j  ], grid[below][right],
+        ];
+
+        if(grid[i][j]["alive"]) livingCells.push(grid[i][j]);
       }
     }
 
     return {
-      'grid': grid,
-      'living': living,
+      "grid": grid,
+      "livingCells": livingCells,
     };
-  }
-  
-  static getLivingNeighbors(grid, x, y) {
-    const h = grid.length;
-    const w = grid[0].length;
-
-    // partial neighbor indices
-    const left =  Galois1.sub(x, 1, w);
-    const right = Galois1.add(x, 1, w);
-    const above = Galois1.sub(y, 1, h);
-    const below = Galois1.add(y, 1, h);
-
-    // neighbor sum
-    return  grid[above][left] + grid[above][x]  + grid[above][right]
-          + grid[  y  ][left]                   + grid[  y  ][right]
-          + grid[below][left] + grid[below][x]  + grid[below][right];
   }
 
   set(x, y, state) {
     this.grid[y][x] = state ? 1 : 0;
   }
 
-  static getDiff(grid, toVisit) {
-    let diff = [];
-    let living = [];
-    
-    for(let i = 0; i < toVisit.length; i++) {
-      const cell = toVisit[i];
-      const x = cell[0], y = cell[1];
+  step() {
+    const diff = [], newLivingCells = [], visited = [];
+    let livingNeighbor;
+    let livingNeighborCount, subLivingNeighborCount;
+    let i, j, k;
 
-      const cellState = grid[y][x];
-      const livingNeighbors = CGOL.getLivingNeighbors(grid, x, y);
+    // visit all unvisited living cells
+    for(i = 0; i < this.livingCells.length; i++) {
+      if(!this.livingCells[i]["visited"]) {
+        this.livingCells[i]["visited"] = true;
+        visited.push(this.livingCells[i]);
 
-      const update = (livingNeighbors == 3)
-        || (cellState && (livingNeighbors == 2 || livingNeighbors == 3))
-        ? 1 : 0;
-
-      if(update) {
-        living.push([x, y]);
+        // eval fate
+        livingNeighborCount = 0;
+        for(j = 0; j < 8; j++) {
+          livingNeighborCount += this.livingCells[i]["neighbors"][j]["alive"];
+        }
+        if(livingNeighborCount != 2 && livingNeighborCount != 3) {
+          diff.push(this.livingCells[i]);
+        }
+        else newLivingCells.push(this.livingCells[i]);
       }
+      
+      // visit each unvisited neighbor
+      for(j = 0; j < 8; j++) {
+        livingNeighbor = this.livingCells[i]["neighbors"][j];
+        if(!livingNeighbor["visited"]) {
+          livingNeighbor["visited"] = true;
+          visited.push(livingNeighbor);
 
-      if(grid[y][x] !== update) {
-        diff.push([x, y, update]);
+          // eval fate
+          livingNeighborCount = 0;
+          for(k = 0; k < 8; k++) {
+            livingNeighborCount +=
+              livingNeighbor["neighbors"][k]["alive"];
+          }
+          if(!livingNeighbor["alive"] && livingNeighborCount == 3) {
+            diff.push(livingNeighbor);
+            newLivingCells.push(livingNeighbor);
+          }
+          else if(livingNeighbor["alive"]
+              && livingNeighborCount != 2
+              && livingNeighborCount != 3) {
+            diff.push(livingNeighbor);
+          }
+          else if(livingNeighbor["alive"]) {
+            newLivingCells.push(livingNeighbor);
+          }
+        }
       }
     }
-    
-    return {
-      'diff': diff,
-      'living': living,
-    };
-  }
 
-  step() {
-    const toVisit = CGOL.findToVisit(this.grid, this.living);
-    const { diff, living } = CGOL.getDiff(this.grid, toVisit);
-    this.diffCells = diff;
-    this.living = living;
-
-
-    for(let i = 0; i < diff.length; i++) {
-      const x = diff[i][0], y = diff[i][1];
-      this.grid[y][x] = diff[i][2];
+    // update and reset
+    this.diff = diff;
+    this.livingCells = newLivingCells;
+    for(i = 0; i < this.diff.length; i++) {
+      this.diff[i]["alive"] ^= 1;
+    }
+    for(i = 0; i < visited.length; i++) {
+      visited[i]["visited"] = false;
     }
   }
 
   print() {
-    for(let i = 0; i < this.diffCells.length; i++) {
-      const x = this.diffCells[i][0], y = this.diffCells[i][1];
-      const cellState = this.grid[y][x];
-      ctx.fillStyle = cellState ? "#fff" : "#000";
+    for(let i = 0; i < this.diff.length; i++) {
+      const x = this.diff[i]["x"], y = this.diff[i]["y"];
+      ctx.fillStyle = this.diff[i]["alive"] ? "#fff" : "#000";
       ctx.fillRect(mult*x, mult*y, mult, mult);
     }
   }
 
-  example() {
-    this.set(1, 0, 1);
-    this.set(2, 1, 1);
-    this.set(2, 2, 1);
-    this.set(1, 2, 1);
-    this.set(0, 2, 1);
-  }
+}
 
+// bad naming, class shares method of the same name
+function start() {
+  const w = Math.floor(document.documentElement.clientWidth / mult);
+  const h = Math.floor(document.documentElement.clientHeight / mult);
+  newCGOL(w, h, freq);
+}
+
+// can improve naming
+function newCGOL(w, h, f) {
+  if(CGOLObj !== undefined) CGOLObj.stop();
+
+  CGOLObj = new CGOL(w, h, f);
+  CGOLObj.start();
+  CGOLObj.play();
+}
+
+function togglePlay() {
+  if(g.isPaused) g.play();
+  else g.pause();
 }
 
 function resizeCanvas() {
@@ -264,22 +238,3 @@ window.onload = () => {
   window.addEventListener("resize", resizeCanvas, false);
   canvas.addEventListener("click", start, false);
 };
-
-function newCGOL(w, h, f) {
-  if(g !== undefined) g.stop();
-
-  g = new CGOL(w, h, f);
-  g.start();
-  g.play();
-}
-
-function togglePlay() {
-  if(g.isPaused) g.play();
-  else g.pause();
-}
-
-function start() {
-  const w = Math.floor(document.documentElement.clientWidth / mult);
-  const h = Math.floor(document.documentElement.clientHeight / mult);
-  newCGOL(w, h, freq);
-}
